d0ef890aeda75bbc9d3fc5c987839c171531c5b2
Flavio Galán <elrohirgt@gmail.com>
2019-12-18 12:15:26 -0600

    Pagination to posts on index added (reloads page)

--- php/functions.php
+++ php/functions.php
@@ -26,6 +26,52 @@ function getURL($directory)
 {
   $dir = explode("\\", $directory);
   $htdocsPosition = array_search("htdocs", $dir, true);
-  $dir = "/".join("/", array_slice($dir, $htdocsPosition+1));
+  $dir = join("/", array_slice($dir, $htdocsPosition+1));
   return $dir;
+}
+
+/* ===============================================================================
+  Description:      Obtiene la respuesta de una API.
+  Parameter(s):     $url  - URL donde hacer la llamada a la API
+                    $method - POST/GET/PUT/DELETE, etc...
+                    $options  - Array para configurar cURL
+                    $values - En caso de no ser tipo GET y llevar valores
+  Requirement(s):   None
+  Return Value(s):  On Success - Response from API
+                    On Failure - Curl Error
+  Author(s):        Flavio Galán
+===============================================================================*/
+function apiCall(String $url, String $method, Array $options = [], Array $values = [])
+{
+  $ch = curl_init($url);
+  $opt = array(
+    CURLOPT_RETURNTRANSFER => true,         // return web page
+    CURLOPT_HEADER         => false,        // don't return headers
+    CURLOPT_FOLLOWLOCATION => true,         // follow redirects
+    CURLOPT_ENCODING       => "",           // handle all encodings
+    CURLOPT_USERAGENT      => "ELROHIRGT",     // who am i
+    CURLOPT_AUTOREFERER    => true,         // set referer on redirect
+    CURLOPT_CONNECTTIMEOUT => 120,          // timeout on connect
+    CURLOPT_TIMEOUT        => 120,          // timeout on response
+    CURLOPT_MAXREDIRS      => 10,           // stop after 10 redirects
+    CURLOPT_POST           => 0,            // i am sending post data
+    CURLOPT_POSTFIELDS     => $values,
+    CURLOPT_SSL_VERIFYHOST => 0,            // don't verify ssl
+    CURLOPT_SSL_VERIFYPEER => false,        //
+    CURLOPT_VERBOSE        => 1                //
+  );
+  if (!empty($options)) {
+    $opt = array_merge($options, $opt);
+  }
+  switch ($method){
+    case 'POST':
+      $opt[CURLOPT_POST] = 1;
+      break;
+  }
+  curl_setopt_array($ch, $opt);
+  $content = curl_exec($ch);
+  if (!$content) {
+    return curl_error($ch);
+  }
+  return json_decode($content);
 }
